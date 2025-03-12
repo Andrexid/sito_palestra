c2e6fa1d42fc8f40efadddfe0c1c7a753702c6da
NicolasBrazzo <nicolasbrazzo8@gmail.com>
2025-03-12 09:19:34 +0100

    Login.php e commenti
    Inserita la pagina login.php per il login, da verificare, inoltre aggiunti i commenti necessari.

--- database/connessione.php
+++ database/connessione.php
@@ -1,11 +1,14 @@
 <?php
+    // Credenziali del database
     $servername = 'localhost';
     $username = 'root';
     $password = ''; // Lascia vuoto se non hai configurato una password
     $dbname = 'palestra';
 
+    // Query di connessione
     $conn = new mysqli($servername, $username, $password, $dbname);
 
+    // controllo della connessione
     if ($conn->connect_error) {
         die('Connection failed: ' . $conn->connect_error);
     } else {
--- php/registrazione.php
+++ php/registrazione.php
@@ -1,58 +1,76 @@
 <?php
 
+// Richiamo della connessione al database
 require_once('../database/connessione.php');
 
-
-$nome = $conn->real_escape_string($_POST['nome']);
-$cognome = $conn->real_escape_string($_POST['cognome']);
+// Recupero variabili tramite POST
+$name = $conn->real_escape_string($_POST['nome']);
+$surname = $conn->real_escape_string($_POST['cognome']);
 $email = $conn->real_escape_string($_POST['email']);
 $psw = $conn->real_escape_string($_POST['password']);
 $vpsw = $conn->real_escape_string($_POST['password-v']);
-$sesso = $conn->real_escape_string($_POST['sesso']);
+$sex = $conn->real_escape_string($_POST['sesso']);
 
-
-if($psw === $vpsw){
+// Doppio controllo della password
+if ($psw === $vpsw) {
     $password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
 } else {
     echo 'Le password non corrispondono, Riprova';
     exit;
 }
 
+// Prima query di controllo 
+$existing_user_control = "SELECT * FROM utenti WHERE email = '$email'";
 
-$sql_control = "SELECT * FROM utenti WHERE email = '$email'";
-$sql = "INSERT INTO utenti (nome, cognome, data_nascita, email, password, sesso, peso, altezza) 
-        VALUES ('$nome', '$cognome', NULL, '$email', '$password_hash', '$sesso', NULL, NULL)";
+// Query di inserimento del nuovo utente
+$insert_new_user = "INSERT INTO utenti (nome, cognome, data_nascita, email, password, sesso, peso, altezza) 
+        VALUES ('$name', '$surname', NULL, '$email', '$password_hash', '$sex', NULL, NULL)";
 
-// verifica se ci sono numeri all'interno di nome o cognome
-if ($result_control = $conn->query($sql_control)) {
+// Primo controllo
+if ($result_control = $conn->query($existing_user_control)) {
+    // Se non ci sono utenti già registrati 
     if ($result_control->num_rows == 0) {
-        if (preg_match("/[0-9]/", $nome)) {
-            echo 'Non inserire numeri nel nome';
+        // verifica se ci sono numeri all'interno di nome o cognome
+        if (preg_match("/[0-9]/", $name)) {
+            $errore = 'Non inserire numeri nel nome';
+            $url = "../utilites/errore-utente.php?errore=" . urlencode($errore);
+            header("Location: $url");
+            exit;
+        } elseif (preg_match("/[0-9]/", $surname)) {
+            $errore = 'Non inserire numeri nel cognome';
+            $url = "../utilites/errore-utente.php?errore=" . urlencode($errore);
+            header("Location: $url");
             exit;
 
-        } elseif (preg_match("/[0-9]/", $cognome)) {
-            echo 'Non inserire numeri nel cognome';
-            exit;
-            // controllo se password è abbastanza lunga
+            // Controllo se password è abbastanza lunga
         } else if ($length = strlen($psw) < 4) {
-            echo 'La password deve contenere più di 5 caratteri';
+            $errore = 'La password deve contenere più di 5 caratteri';
+            $url = "../utilites/errore-utente.php?errore=" . urlencode($errore);
+            header("Location: $url");
         } else {
-            if ($conn->query($sql) === true) {
+            if ($conn->query($insert_new_user) === true) {
                 echo "Registrazione effettuata con successo";
 
+                // Inizializzazione della sessione
                 session_start();
 
                 $_SESSION['id'] = $conn->insert_id;
-                $_SESSION['loggato'] = true;
+                $_SESSION['logged'] = true;
 
+                // Spostamneto nell'account
                 header('Location: account.php');
             } else {
-                echo 'Errore durante la registrazione dell\'utente';
+                $errore = 'Errore durante la registrazione dell\'utente';
+                $url = "../utilites/errore-utente.php?errore=" . urlencode($errore);
+                header("Location: $url");
             }
         }
     } else {
-        echo 'Utente già registrato';
+        $errore = 'Utente già registrato';
+        $url = "../utilites/errore-utente.php?errore=" . urlencode($errore);
+        header("Location: $url");
     }
 }
 
-$conn->close();
+// Richiamo della chiusura della connessione al database
+require_once('../database/close-connessione.php');
