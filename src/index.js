f07f01d83bc9f423cb74fe1fe2975a670bbdb067
AndreSabe <andreasabetta456@gmail.com>
2025-04-10 10:01:52 +0200

    Sistemato tutto

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function controllaAccesso(destination){
     if(localStorage.getItem("email")){
-        window.location.href = "html/" + destination;
+        window.location.href = destination;
     }else{
         window.location.href = "html/login.html"
     }
