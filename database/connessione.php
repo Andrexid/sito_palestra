b61970e70285dc54bb20f3d959dbc743e7823fbb
AndreSabe <andreasabetta456@gmail.com>
2025-05-03 12:57:33 +0200

    aggiunta connessione db giusto

--- database/connessione.php
+++ database/connessione.php
@@ -1,9 +1,14 @@
 <?php
     // Credenziali del database
-    $servername = 'localhost';
-    $username = 'root';
-    $password = ''; // Lascia vuoto se non hai configurato una password
-    $dbname = 'palestra';
+    // $servername = 'localhost';
+    // $username = 'root';
+    // $password = ''; // Lascia vuoto se non hai configurato una password
+    // $dbname = 'palestra';
+
+    $servername = 'mygymscpalestra.mysql.db';
+    $username = 'mygymscpalestra';
+    $password = 'Detectiveconan05'; // Lascia vuoto se non hai configurato una password
+    $dbname = 'mygymscpalestra';
 
     // Query di connessione
     $conn = new mysqli($servername, $username, $password, $dbname);
